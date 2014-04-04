d2cd7d4eef975fa399f96f298d7b59f69597e9da
Jon Schipp <jonschipp@gmail.com>
2014-04-03 23:59:18 -0500

    do not use https address for mayhemic list, no reason to

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ malcachesnoop
 Compare an updated malhost list to a DNS servers cache. <br>
 Used to check internal DNS servers for signs of mal-ware on the network.
 
-`malcachesnoop` downloads the latest [Mayhemic Labs](https://secure.mayhemiclabs.com/malhosts/malhosts.txt) list
+`malcachesnoop` downloads the latest [Mayhemic Labs](http://secure.mayhemiclabs.com/malhosts/malhosts.txt) list
 and uses that to check for entries in a DNS server's cache looking for the presence of malware.
 
 ## Example:
--- malcachesnoop.sh
+++ malcachesnoop.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-# BSD License: 
+# BSD License:
 # Copyright (c) 2013, Jon Schipp
 # All rights reserved.
-# 
-# Redistribution and use in source and binary forms, with or without modification, 
+#
+# Redistribution and use in source and binary forms, with or without modification,
 # are permitted provided that the following conditions are met:
 #
-# Redistributions of source code must retain the above copyright notice, this list of 
+# Redistributions of source code must retain the above copyright notice, this list of
 # conditions and the following disclaimer. Redistributions in binary form must reproduce
 # the above copyright notice, this list of conditions and the following disclaimer in the
 # documentation and/or other materials provided with the distribution.
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
-# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
-# SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+# SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
 # INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 # Check for argument
@@ -30,7 +30,7 @@ fi
 ns=$(dig -x @$1 -x "$1" +short)
 
 # d/l latest malware host list
-curl --insecure -O https://secure.mayhemiclabs.com/malhosts/malhosts.txt &>/dev/null
+curl -O http://secure.mayhemiclabs.com/malhosts/malhosts.txt &>/dev/null
 
 # Initialize counter for array
 n=1
@@ -51,7 +51,7 @@ ipdb[$n]=$(dig @$1 "$name" +norecurse +short | head -1)
 # malhosts.txt is read in here
 done < <(cut -f1 < malhosts.txt | sed -e '/^#/d' -e '/^$/d')
 
-# Count indexes with names 
+# Count indexes with names
 found=$(echo $((${#ipdb[@]}-1)))
 
 # Print heading
